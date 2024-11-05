8a4b2272ea457d05c39d6217a2715783d93487ef
Shiva Tiwari <workshop@Shivas-MacBook-Pro.local>
2024-11-06 01:21:32 +0200

    mobile number

--- src/components/ContactForm/contact.js
+++ src/components/ContactForm/contact.js
@@ -8,14 +8,15 @@ const Contact = () => {
       <div>
         <h2>Contact us</h2>
         <p>RecordKeep Oy</p>
-{/*         <div className="email-div">
+        {/*         <div className="email-div">
           <p>Email :</p>
           <a className="link" href="mailto:gopalkandel@gmail.com">
             <p>gopalkandel@gmail.com</p>
           </a>
         </div> */}
 
-        <p>Office address : Vilhonvuorenkatu 12, 00500 Helsinki</p>
+        <p>Contact : +358503797561</p>
+        <p>Address : Vilhonvuorenkatu 12, 00500 Helsinki</p>
       </div>
     </div>
   );
